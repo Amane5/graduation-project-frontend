9b16854cd4330d3bc072f2870c6f30bd1eb2d069
Amani Tanan <amany.tan2004@gmail.com>
2026-06-16 13:53:33 +0300

    update repo name in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "graduation-project-frontend",
+  base: "/graduation-project-frontend/",
   server: {
     host: "::",
     port: 8080,
