21d0db86ced7fce97140802a448ee77f12199b56
Amani Tanan <amany.tan2004@gmail.com>
2026-06-16 13:43:56 +0300

    update repository name

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "kidspark-ai",
+  base: "graduation-project-frontend",
   server: {
     host: "::",
     port: 8080,
